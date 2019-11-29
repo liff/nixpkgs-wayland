cf191e3518f0631613bbea614aa62fde105abc64
Cole Botkens <cole.mickens+colebot@gmail.com>
2019-11-29 19:08:21 +0000

    auto-updates wip

--- nixpkgs/nixpkgs-unstable/metadata.nix
+++ nixpkgs/nixpkgs-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs";
   branch = "nixpkgs-unstable";
   url = "https://github.com/nixos/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-  rev = "58fb23f72ad916c8bbfa3c3bc2d0c83c9cfcdd16";
-  sha256 = "0f7gnsis5hdrdcmiv7s06qz02pszmmfxrqvc77jf0lmc86i25x9i";
-  revdate = "2019-11-20 08:40:07 +0100";
+  rev = "33e6bbde5915250249cbeb4310ef95e75507eaad";
+  sha256 = "0g660w2i4ds334jbjarw9paz6hn4x99ylbkh825nvc357pvv9y80";
+  revdate = "2019-11-29 13:25:48 +0000";
 }
--- pkgs/oguri/metadata.nix
+++ pkgs/oguri/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/vilhalmer/oguri";
   branch = "master";
-  rev = "4c3fc5c4191231770b402475adc1fba5362d10dd";
-  sha256 = "0srlqyy7w0j220d7vkcscwkd03fcappwd05jcy05dv7kxk7cymrz";
-  revdate = "2019-11-18 15:47:39 -0500";
+  rev = "88746bf6395b275ff70983de71d0ae2702856a5e";
+  sha256 = "00kqq8mkbxk6mvzc16l1633nw6rjah6n9kwifvjnm7asfa3md7nf";
+  revdate = "2019-11-26 10:58:26 -0500";
 }
